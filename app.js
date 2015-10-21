b9b265532a330b4f5b7e90c17d52ff2f0dfe40eb
Jenny Knuth <inkboco@gmail.com>
2015-10-21 09:06:57 -0600

    commented version

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var margin = { top: 30, right: 10, bottom: 30, left: 30 },
     width = 1000 - margin.left - margin.right,
     gridSize = Math.floor(width / 53),
     height = (gridSize * 10) - margin.top - margin.bottom,
-    legendElementWidth = gridSize * 3,
+    // legendElementWidth = gridSize * 3,
     days = ["M", "T", "W", "T", "F"],
     weeks = ["", "J", "", "", "", "F", "", "", "", "M", "", "", "", "", "A", "", "", "", "M", "", "", "", "J", "", "", "", "", "J", "", "", "", "A", "", "", "", "S", "", "", "", "", "O", "", "", "", "N", "", "", "", "D", "", "", "", "" ],
     values = ['hike', 'bike', 'swim', 'run', 'stretch', 'climb', 'lift'],
@@ -67,45 +67,46 @@ var margin = { top: 30, right: 10, bottom: 30, left: 30 },
     var heatMap = svg.selectAll(".grid")
         .data([1])
         .enter().append("rect")
-        // .attr("x", function(d) { return (d.week - 1) * gridSize; })
-        // .attr("y", function(d) { return (d.day - 1) * gridSize; })
-        // .attr("rx", 4)
-        // .attr("ry", 4)
-        // .attr("class", "bordered")
         .attr("width", gridSize)
         .attr("height", gridSize)
+        .attr("x", function(d) { return (d.week - 1) * gridSize; })
+        .attr("y", function(d) { return (d.day - 1) * gridSize; })
+        .attr("rx", 4)
+        .attr("ry", 4)
+        .attr("class", "bordered")
         // .style("fill", function(d) { return colorScale(d.type);})
         .style("fill", colors[0]);
 
-    // heatMap.transition().duration(1000)
-    //     .style("fill", function(d) { return colorScale(d.type); })
-    //     .style("fill-opacity", "60%");
-    //
-    // heatMap.append("title").text(function(d) {
-    //   var title = d.type + ' ' + d.date
-    //   return title; });
-
-    // var legend = svg.selectAll(".legend")
-    //     .data(colorScale.domain(), function(d) { return d; })
-    //     .enter().append("g")
-    //     .attr("class", "legend");
-    //
-    // legend.append("rect")
-    //   .attr("x", function(d, i) { return legendElementWidth * i; })
-    //   .attr("y", gridSize * 6)
-    //   .attr("width", legendElementWidth)
-    //   .attr("height", gridSize)
-    //   .attr("class", "bordered")
-    //   .attr("rx", 4)
-    //   .attr("ry", 4)
-    //   .style("fill", function(d, i) { return colors[i]; })
-    //   .style("fill-opacity", "60%");
-    //
-    // legend.append("text")
-    //   .attr("class", "mono")
-    //   .text(function(d) { return d; })
-    //   .attr("x", function(d, i) { return (legendElementWidth * i) + legendElementWidth/2; })
-    //   .attr("y", (gridSize * 6) + (gridSize/1.4))
-    //   .style("text-anchor", "middle");
-
+//     heatMap.transition().duration(1000)
+//         .style("fill", function(d) { return colorScale(d.type); })
+//         .style("fill-opacity", "60%");
+//
+//     heatMap.append("title").text(function(d) {
+//       var title = d.type + ' ' + d.date
+//       return title; });
+//
+//     var legend = svg.selectAll(".legend")
+//         .data(colorScale.domain(), function(d) { return d; })
+//         .enter().append("g")
+//         .attr("class", "legend");
+//
+//     legend.append("rect")
+//       .attr("x", function(d, i) { return legendElementWidth * i; })
+//       .attr("y", gridSize * 6)
+//       .attr("width", legendElementWidth)
+//       .attr("height", gridSize)
+//       .attr("class", "bordered")
+//       .attr("rx", 4)
+//       .attr("ry", 4)
+//       .style("fill", function(d, i) { return colors[i]; })
+//       .style("fill-opacity", "60%");
+//
+//
+//     legend.append("text")
+//       .attr("class", "mono")
+//       .text(function(d) { return d; })
+//       .attr("x", function(d, i) { return (legendElementWidth * i) + legendElementWidth/2; })
+//       .attr("y", (gridSize * 6) + (gridSize/1.4))
+//       .style("text-anchor", "middle");
+//
 // });
